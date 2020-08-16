df9529796d95ac8ead892e86563e613762ac6b51
Suho Lee <susemeee@gmail.com>
2020-08-16 21:24:57 +0900

    Add port as env

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
 
 import Cryptersuite from './cryptersuite/index.ts';
 
-Cryptersuite(Deno.env.get('APPROVE_KEY') ?? '');
+Cryptersuite(Deno.env.get('APPROVE_KEY') ?? '', Number(Deno.env.get('PORT') ?? '8080'));
